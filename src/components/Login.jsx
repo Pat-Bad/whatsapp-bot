06caec0ba8d33f1d6ddb7a27783bd1e2450ca6cb
Patricia <patricia.badji89@gmail.com>
2025-05-10 00:54:44 +0200

    creates simple login form to link to be

---
+++ src/components/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
+
+const Login = () => {
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+
+    setTimeout(() => {
+      // Da collegare a backend, lo faccio in intellij?
+    }, 1000);
+  };
+
+  return (
+    <Container className="container-fluid d-flex">
+      <Row>
+        <Col
+          col-12
+          className="d-flex justify-content-center"
+        >
+          <Form onSubmit={handleLogin}>
+            <Form.Group
+              controlId="formUsername"
+              className="mb-3"
+            >
+              <Form.Label style={{ marginRight: "10px" }}>Username</Form.Label>
+              <Form.Control
+                type="text"
+                name="username"
+                placeholder="Inserisci username"
+                value={loginData.username}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </Form.Group>
+
+            <Form.Group
+              controlId="formPassword"
+              className="mb-3"
+            >
+              <Form.Label style={{ marginRight: "10px" }}>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Inserisci password"
+                value={loginData.password}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </Form.Group>
+
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-100 mt-3"
+              disabled={loading}
+            >
+              {loading ? "Accesso in corso..." : "Login"}
+            </Button>
+          </Form>
+
+          {error && (
+            <Alert
+              variant="danger"
+              className="mt-3"
+            >
+              {error}
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/components/Manager.jsx
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Manager = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Manage your users</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Manager;
